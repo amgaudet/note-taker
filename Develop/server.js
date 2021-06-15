d72c72f5e5af6cb7a9dd47d0a05cd225e6f373c9
Shreekz <alanmguadet@gmail.com>
2021-06-15 14:40:48 -0400

    fixed directory callings

--- Develop/server.js
+++ Develop/server.js
@@ -9,9 +9,9 @@ app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 app.use(express.static('public'));
 
-app.get('/*', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './public/notes.html')));
 app.get('/api/notes', (req, res) => res.sendFile(path.join(__dirname, './db/db.json')));
+app.get('/*', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
 
 app.post('/api/notes', (req, res) => {
     const body = {...req.body};
